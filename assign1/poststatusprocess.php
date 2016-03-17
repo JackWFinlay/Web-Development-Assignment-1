d02506a08b0bc3c15b07640704ede8a33127cc3e
Jack Witbrock Finlay <jack.witbrock@gmail.com>
2016-03-18 09:42:22 +1300

    update postsstatusprocess.php to handle empty params

--- assign1/poststatusprocess.php
+++ assign1/poststatusprocess.php
@@ -19,31 +19,56 @@
 		<div class="container">
 			<h1>Status Posting System</h1>
 			<?php
-				// Regular Expressions
-				$statusCodePattern = '/^S\d{4}$/';
-				$statusPattern     = '/^[a-zA-Z0-9\s!,\?\.]*$/';
-				$datePattern       = '/^\d{1,2}\/\d{1,2}\/\d{4}$/';
+				function processStatus() {
+					// Regular Expressions
+					$statusCodePattern = '/^S\d{4}$/';
+					$statusPattern     = '/^[a-zA-Z0-9\s!,\?\.]*$/';
+					$datePattern       = '/^\d{1,2}\/\d{1,2}\/\d{4}$/';
 
-				$statusCode   = $_POST["statusCode"];
-				$status       = $_POST["status"];
-				$date         = $_POST["date"];
-				$share        = isset($_POST["share"]) ? $_POST["share"] : "Public"; //Default to public if not set
-				$allowLike	  = isset($_POST["allowLike"]) ? $_POST["allowLike"] : FALSE; //FALSE if not set;
-				$allowComment = isset($_POST["allowComment"]) ? $_POST["allowComment"] : FALSE;
-				$allowShare   = isset($_POST["allowShare"]) ? $_POST["allowShare"] : FALSE;
+					if (empty($_POST["statusCode"])){ // Print message if statusCode is not specified.
+						echo "<p>Status Code not specified</p>";
+					}
 
-				// Check that code, status, and date match patterns
-				if ((preg_match($statusCodePattern, $statusCode)) &&
-					(preg_match($statusPattern, $status)) &&
-				 	(preg_match($datePattern, $date))) {
+					if (empty($_POST["status"])){
+						echo "<p>Status not specified</p>";
+					}
 
-				 	echo "<p>$statusCode, $status, $date<p>";
-				} else {
-					echo "<p>Failure.</p>";
+					if (empty($_POST["date"])){
+						echo "<p>Date not specified</p>";
+					}
+
+					if (empty($_POST["statusCode"]) ||
+						empty($_POST["status"]) ||
+						empty($_POST["date"])){ // Display link back to poststatus form if any value is empty.
+
+						echo "<a href='poststatusform.php' class='display-block'>Try again</a><br/>";
+						return; // Stop processing php section.
+					} 
+
+
+					$statusCode   = $_POST["statusCode"]; 
+					$status       = $_POST["status"];
+					$date         = $_POST["date"];
+					$share        = isset($_POST["share"]) ? $_POST["share"] : "Public"; //Default to public if not set
+					$allowLike	  = isset($_POST["allowLike"]) ? $_POST["allowLike"] : FALSE; //FALSE if not set;
+					$allowComment = isset($_POST["allowComment"]) ? $_POST["allowComment"] : FALSE;
+					$allowShare   = isset($_POST["allowShare"]) ? $_POST["allowShare"] : FALSE;
+
+					// Check that code, status, and date match patterns
+					if ((preg_match($statusCodePattern, $statusCode)) &&
+						(preg_match($statusPattern, $status)) &&
+					 	(preg_match($datePattern, $date))) {
+
+					 	echo "<p>$statusCode, $status, $date<p>";
+					} else {
+						echo "<p>Failure.</p>";
+					}
 				}
 
+				processStatus();
 			?>
-
+			
+			<a href="index.php">Return to Home Page</a>
 		</div>
 	</body>
 </html>
