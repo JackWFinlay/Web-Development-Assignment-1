57f034642f42ccea27e676dd94bc17c2075b9430
Jack Witbrock Finlay <jack.witbrock@gmail.com>
2016-03-19 16:01:21 +1300

    Update About page.

--- assign1/about.php
+++ assign1/about.php
@@ -20,27 +20,40 @@
 			<h1>Status Posting System</h1>
 			
 			<ul>
-				<li>I attempted to use the Bootstrap framework, but found that it didn't really add anything I couldn't just do myself. I also found that it would not allow me to really show off my skills in CSS as most of it is already done by the framework.</li>
-				<li>I had trouble getting some of the database stuff to work. I have not worked with a MySQL database or PHP before, so it was an interesting challenge.</li>
-				<li>I would like to make a much better looking site, with better styling and a more interesting layout.</li>
-				<li>I have used the following resources:
-					<ul>
-						<li>The lecture slides.</li>
-						<li>
-							<a href="http://w3schools.com" target="_blank">w3schools.com
-							</a>
-						</li>
-						<li>
-							<a href="http://stackoverflow.com" target="_blank">stackoverflow.com
-							</a>
-						</li>
-						<li>
-							<a href="http://php.net" target="_blank">php.net
-							</a>
-						</li>
-					</ul>
+				<li>
+					<p>I attempted to use the Bootstrap framework, but found that it didn't really add anything I couldn't just do myself. I also found that it would not allow me to really show off my skills in CSS as most of it is already done by the framework.</p>
+
+					<p>I abstracted some common functions out to the file called 'helperfunctions.php'. This helped me to maintain and reuse some functions in different sections. It also simplified the code in the individual pages by a large amount.</p>
+
 				</li>
-				<li>Although I have quite a lot of experience with web development I found that I still learned a lot. There were things that I did not know about, such as the reset input element, and how to connect a database to a webpage in PHP. I also learned how to use PHP, I have previous experience with ASP.NET MVC and so it was quite an interesting change.</li>
+				<li>
+					<p>I had trouble getting some of the database stuff to work. I have not worked with a MySQL database or PHP before, so it was an interesting challenge.</p>
+				</li>
+				<li>
+					<p>I would like to make a much better looking site, with better styling and a more interesting layout.</p>
+				</li>
+				<li>
+					<p>I have used the following resources:
+						<ul>
+							<li>The lecture slides.</li>
+							<li>
+								<a href="http://w3schools.com" target="_blank">w3schools.com
+								</a>
+							</li>
+							<li>
+								<a href="http://stackoverflow.com" target="_blank">stackoverflow.com
+								</a>
+							</li>
+							<li>
+								<a href="http://php.net" target="_blank">php.net
+								</a>
+							</li>
+						</ul>
+					</p>
+				</li>
+				<li>
+					<p>Although I have quite a lot of experience with web development I found that I still learned a lot. There were things that I did not know about, such as the reset input element, and how to connect a database to a webpage in PHP. I also learned how to use PHP, I have previous experience with ASP.NET MVC and so it was quite an interesting change.</p>
+				</li>
 			</ul>
 
 			<br/>
